3326338d331cfd5c666475f7e172d5d5a25749bb
ImYoungBin <sys02115@naver.com>
2022-08-07 18:20:13 +0900

    add problems in code

--- python/valid_parentheses.py
+++ python/valid_parentheses.py
@@ -1,5 +1,12 @@
-from os import lseek
+'''
+Write a function that takes a string of parentheses, and determines if the order of the parentheses is valid. The function should return true if the string is valid, and false if it's invalid.
 
+Examples
+"()"              =>  true
+")(()))"          =>  false
+"("               =>  false
+"(())((()())())"  =>  true
+'''
 
 def valid_parentheses(string):
     lst = [ string[i] for i in range(len(string)) if (string[i] == '(') | (string[i] == ')')]
